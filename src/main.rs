f0101918848f069e65a0397424e9686397297948
Nelu Platonov <nelu.platonov@gmail.com>
2025-07-02 16:40:41 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::env;
 use std::io;
 use std::process;
 
+const DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        input_line.contains(pattern)
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+    match pattern { 
+        "\\d" => { input_line.contains(DIGITS) },
+        char => { input_line.contains(char) }
     }
 }
 
