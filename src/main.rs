3519632c93e8e7d740ee9c2930fb7a100366ac72
Nelu Platonov <nelu.platonov@gmail.com>
2025-07-02 20:05:30 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,51 @@ use std::env;
 use std::io;
 use std::process;
 
-const DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const ASCII_0: u8 = 48;
+const ASCII_9: u8 = 57;
+const ASCII_UPPERCASE_A: u8 = 65;
+const ASCII_UPPERCASE_Z: u8 = 90;
+const ASCII_LOWERCASE_A: u8 = 97;
+const ASCII_LOWERCASE_Z: u8 = 122;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    match pattern { 
-        "\\d" => { input_line.contains(DIGITS) },
-        char => { input_line.contains(char) }
+    match pattern {
+        "\\d" => {
+            input_line.chars()
+                .find(|&char| char_is_digit(char))
+                .is_some()
+        },
+        
+        "\\w" => {
+            input_line.chars()
+                .find(|&char| char_is_digit(char) || char_is_letter(char))
+                .is_some()
+        }
+        char => input_line.contains(char),
     }
 }
 
-fn main() {
-    eprintln!("Logs from your program will appear here!");
+#[inline(always)]
+fn char_is_letter(char: char) -> bool {
+    char_is_uppercase_letter(char) || char_is_lowercase_letter(char)
+}
 
+#[inline(always)]
+fn char_is_uppercase_letter(char: char) -> bool {
+    char as u8 >= ASCII_UPPERCASE_A && char as u8 <= ASCII_UPPERCASE_Z
+}
+
+#[inline(always)]
+fn char_is_lowercase_letter(char: char) -> bool {
+    char as u8 >= ASCII_LOWERCASE_A && char as u8 <= ASCII_LOWERCASE_Z
+}
+
+#[inline(always)]
+fn char_is_digit(char: char) -> bool {
+    char as u8 >= ASCII_0 && char as u8 <= ASCII_9
+}
+
+fn main() {
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
